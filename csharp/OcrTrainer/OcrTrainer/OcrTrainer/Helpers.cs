afedb91812ed50d9fad6509129e848c1c84e92ea
Wojtek Denejko <deneyw@gmail.com>
2016-12-05 22:26:09 +0100

    ocrTrainer

---
+++ csharp/OcrTrainer/OcrTrainer/OcrTrainer/Helpers.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+using UPImage;
+
+namespace OcrTrainer
+{
+    public class Helpers
+    {
+        public List<InputPattern> GetPatternsFromBitmap(InputPattern parentPattern, int hStep, int vStep, bool isTopStart, int minWidth, int minHeight)
+        {
+            List<InputPattern> patternlist = null;
+            parentPattern.GetPatternBoundaries(hStep, vStep, isTopStart, minWidth, minHeight);
+            if (parentPattern.PatternRects.Count > 0)
+            {
+                patternlist = new List<InputPattern>();
+                foreach (var rect in parentPattern.PatternRects)
+                {
+                    var bmp = new Bitmap(rect.Width, rect.Height);
+                    var graph = Graphics.FromImage(bmp);
+                    graph.DrawImage(parentPattern.OriginalBmp, 0, 0, rect, GraphicsUnit.Pixel);
+                    var x = parentPattern.OriginalRectangle.X + rect.X;
+                    var y = parentPattern.OriginalRectangle.Y + rect.Y;
+                    var newRect = new Rectangle(x, y, rect.Width, rect.Height);
+                    var childPattern = new InputPattern(bmp, 255, newRect);
+                    patternlist.Add(childPattern);
+                    graph.Dispose();
+                }
+            }
+
+            return patternlist;
+        }
+
+        public BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                var bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
---
+++ csharp/OcrTrainer/OcrTrainer/OcrTrainer/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using UPImage;
+
+namespace OcrTrainer
+{
+    public partial class MainWindow
+    {
+        private Bitmap _drawBitmap;
+        private Bitmap _bitmap;
+        private Bitmap _graySourceBitmap;
+        public Helpers Helpers { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openfile = new OpenFileDialog();
+            this.Helpers = new Helpers();
+            openfile.ShowDialog();
+            if (!openfile.CheckFileExists) return;
+            var originalImage = new Image<Bgr, byte>(openfile.FileName);
+            //         var originalResized = originalImage.Resize(1024, 768, Inter.Linear);
+            SourceImage.Source = this.Helpers.BitmapToImageSource(originalImage.Bitmap);
+            var originalGrayImage = new Image<Gray, byte>(openfile.FileName);
+            //           var greyResized = originalGrayImage.Resize(1024, 768, Inter.Linear);
+            GrayScaleImage.Source = this.Helpers.BitmapToImageSource(originalGrayImage.Bitmap);
+            _graySourceBitmap = originalGrayImage.Bitmap;
+        }
+
+        private void CharacterRecognizeButton_Click(object sender, EventArgs e)
+        {
+            if (_bitmap != null && this.Helpers != null)
+            {
+                _bitmap.Dispose();
+                _bitmap = null;
+            }
+            _bitmap = new Bitmap(_graySourceBitmap.Width, _graySourceBitmap.Height);
+            _drawBitmap = (Bitmap)_graySourceBitmap.Clone();
+            if (_bitmap == null || this.Helpers == null) return;
+            var parentPt = new InputPattern(_graySourceBitmap, 255, new Rectangle(0, 0, _bitmap.Width, _bitmap.Height));
+            var lineList = this.Helpers.GetPatternsFromBitmap(parentPt, 500, 1, true, 10, 10);
+            if (lineList.Count > 0)
+            {
+                foreach (var line in lineList)
+                {
+                    var wordList = this.Helpers.GetPatternsFromBitmap(line, 50, 10, false, 10, 10);
+                    if (!(wordList?.Count > 0)) continue;
+                    foreach (var word in wordList)
+                    {
+                        var charList = this.Helpers.GetPatternsFromBitmap(word, 5, 5, false, 40, 40);
+                        if (!(charList?.Count > 0)) continue;
+                        foreach (var c in charList)
+                        {
+                            c.GetPatternBoundaries(5, 5, false, 40, 40);
+                            _drawBitmap = c.DrawChildPatternBoundaries(_drawBitmap);
+                            c.OriginalBmp.Save(@"PATH");
+                        }
+                    }
+                }
+            }
+            PbPreview.Source = this.Helpers.BitmapToImageSource(_drawBitmap);
+        }
+    }
+}
